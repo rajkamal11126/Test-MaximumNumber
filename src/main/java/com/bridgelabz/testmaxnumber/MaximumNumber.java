55a1a160d92ae6a8ef20e1087929678449994750
Raj <rajkamal11126@gmail.com>
2020-10-27 12:50:09 +0530

    Refactor : Use Generic method to find max value

--- src/main/java/com/bridgelabz/testmaxnumber/MaximumNumber.java
+++ src/main/java/com/bridgelabz/testmaxnumber/MaximumNumber.java
@@ -1,8 +1,8 @@
 package com.bridgelabz.testmaxnumber;
 
 public class MaximumNumber {
-	// UC-1 comparing three integer numbers
-	public static Integer maxNumber(Integer x, Integer y, Integer z) {
+
+	public static <T extends Comparable<T>> T compare (T x, T y, T z) {
 		if (x.compareTo(y) > 0 && x.compareTo(z) > 0) {
 			System.out.println("Maximum number is:" + x);
 		} else if (y.compareTo(z) > 0) {
@@ -13,27 +13,5 @@ public class MaximumNumber {
 		return z;
 	}
 
-	// UC-2 comparing three float numbers
-	public static Float maxNumber(Float x, Float y, Float z) {
-		if (x.compareTo(y) > 0 && x.compareTo(z) > 0) {
-			System.out.println("Maximum number is:" + x);
-		} else if (y.compareTo(z) > 0) {
-			System.out.println("Maximum number is:" + y);
-		} else {
-			System.out.println("Maximum number is:" + z);
-		}
-		return z;
-	}
-
-	// UC-3 comparing three string
-	public static String maxNumber(String s1, String s2, String s3) {
-		if (s1.compareTo(s2) > 0 && s1.compareTo(s3) > 0) {
-			System.out.println("Maximum number is:" + s1);
-		} else if (s2.compareTo(s3) > 0) {
-			System.out.println("Maximum number is:" + s2);
-		} else {
-			System.out.println("Maximum number is:" + s3);
-		}
-		return s3;
-	}
+	
 }
--- src/test/java/com/bridgelabz/testmaxnumber/MaximumNumberTest.java
+++ src/test/java/com/bridgelabz/testmaxnumber/MaximumNumberTest.java
@@ -7,63 +7,63 @@ public class MaximumNumberTest {
 	// TC-1.1
 	@Test
 	public void givenMaxIntegerNumberAtFirstPosition_WhenProper_ShouldReturnSameNumber() {
-		Integer result = MaximumNumber.maxNumber(45, 32, 21);
+		Integer result = MaximumNumber.compare(45, 32, 21);
 		Assert.assertEquals((Integer) 45, result);
 	}
 
 	// TC-1.2
 	@Test
 	public void givenMaxIntegerNumberAtSecondPosition_WhenProper_ShouldReturnSameNumber() {
-		Integer result = MaximumNumber.maxNumber(20, 60, 21);
+		Integer result = MaximumNumber.compare(20, 60, 21);
 		Assert.assertEquals((Integer) 60, result);
 	}
 
 	// TC-1.3
 	@Test
 	public void givenMaxIntegerNumberAtThirdPosition_WhenProper_ShouldReturnSameNumber() {
-		Integer result = MaximumNumber.maxNumber(60, 70, 80);
+		Integer result = MaximumNumber.compare(60, 70, 80);
 		Assert.assertEquals((Integer) 80, result);
 	}
 
 	// TC-2.1
 	@Test
 	public void givenMaxFloatNumberAtFirstPosition_WhenProper_ShouldReturnSameNumber() {
-		Float result = MaximumNumber.maxNumber(45.5f, 32.5f, 21.5f);
+		Float result = MaximumNumber.compare(45.5f, 32.5f, 21.5f);
 		Assert.assertEquals((Float) 45f, result);
 	}
 
 	// TC-2.2
 	@Test
 	public void givenMaxFlatNumberAtSecondPosition_WhenProper_ShouldReturnSameNumber() {
-		Float result = MaximumNumber.maxNumber(20.3f, 60.5f, 21.6f);
+		Float result = MaximumNumber.compare(20.3f, 60.5f, 21.6f);
 		Assert.assertEquals((Float) 60.5f, result);
 	}
 
 	// TC-2.3
 	@Test
 	public void givenMaxFloatNumberAtThirdPosition_WhenProper_ShouldReturnSameNumber() {
-		Float result = MaximumNumber.maxNumber(60.1f, 70.2f, 80.3f);
+		Float result = MaximumNumber.compare(60.1f, 70.2f, 80.3f);
 		Assert.assertEquals((Float) 80.3f, result);
 	}
 
 	// TC-3.1
 	@Test
 	public void givenMaxStringAtFirstPosition_WhenProper_ShouldReturnSameNumber() {
-		String result = MaximumNumber.maxNumber("Wipro", "Bridgelabz", "Cognizant");
+		String result = MaximumNumber.compare("Wipro", "Bridgelabz", "Cognizant");
 		Assert.assertEquals("Wipro", result);
 	}
 
 	// TC-3.2
 	@Test
 	public void givenMaxStringAtSecondPosition_WhenProper_ShouldReturnSameNumber() {
-		String result = MaximumNumber.maxNumber("Blue", "White", "Green");
+		String result = MaximumNumber.compare("Blue", "White", "Green");
 		Assert.assertEquals("White", result);
 	}
 
 	// TC-3.3
 	@Test
 	public void givenMaxStringAtThirdPosition_WhenProper_ShouldReturnSameNumber() {
-		String result = MaximumNumber.maxNumber("Apple", "Banana", "Peach");
+		String result = MaximumNumber.compare("Apple", "Banana", "Peach");
 		Assert.assertEquals("Peach", result);
 	}
 }
